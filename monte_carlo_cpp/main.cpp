4dfa935121da980e78d0cbeea8b82615ac9b25fd
brianreicher <reicher.b@northeastern.edu>
2022-05-26 21:06:10 -0400

    Implement original Monte Carlo algo from python

--- monte_carlo_cpp/main.cpp
+++ monte_carlo_cpp/main.cpp
@@ -1,65 +1,45 @@
 #include <iostream>
-#include <stdio.h>
-#include <cstdlib>
-#include <cmath>
+#include <math.h>
 
-double rnd();
+/*
+y=[]
+for i in range(1,1001):
+    I=0.
+    n0=i
+    N=n0
+    r=np.random.random(n0)
+    for i in range(n0):
+        x=r[i]
+        I+=np.sqrt(1-x**2)
+    I*=4./float(n0)
+    y.append(I)
+x=np.arange(1,1001,1)
+ */
 
-double function (double x);
 
-void Monte_Carlo_1 (long* s);
-void Monte_Carlo_2 (long* d);
-void Monte_Carlo_3 (double x, long* f);
+float simpleMonteCarlo(int range,  float i = 0.)
+{
+    float n_o = i;
+    int randArr[range];
+    for (int j = 0; j < range; j++) {
+        randArr[j] = rand() % 1;
+    }
+    for (int k = 0; k < range; k++)
+    {
+        int x = randArr[k];
+        i += sqrt(1- pow(x, 2));
+    }
+    i *= 4./n_o;
+
+    return i;
+}
+
 
 int main()
 {
-    double x;
-    long max = 5, n = 0, s = 0, d = 0, f = 0;
-    printf("\n*** Integralas ( I - 0.5 ): Monte Karlo metodas ***\n");
-    printf("\nTikslus rezultatas = 0.0250 \n \n");
-    printf("Iter. \t I \t II \t III \n \n");
-
-    for (int j=0; j < 13; j++)
-    {
-        for (int i = 0; i < max; i++)
-        {
-            Monte_Carlo_1(&s);
-            Monte_Carlo_2(&d);
-            Monte_Carlo_3(x,&f);
-            n++;
-        }
-        printf("%6ld %7.4lf %7.4lf %7.4lf \n", max, (double)s / n - 0.5, (double)d / n - 0.5, f / n - 0.5);
-        max *= 2;
-    }
-}
-
-double rnd()
-{
-    return (double)rand() / ( RAND_MAX );
-}
-
-void Monte_Carlo_1 (long* s)
-{
-    double x = rnd();
-    double y = rnd();
-    if(y < function(x))
-    {
-        (*s)++;
-    }
-}
-
-void Monte_Carlo_2 (long* d)
-{
-    (*d) += rnd();
-}
-
-void Monte_Carlo_3 (double x, long* f)
-{
-    x = sqrt(rnd());
-    *f += (x / (1 + 0.3 * x * (1 - x)) / x);
-}
-
-double function (double x)
-{
-    return (x + 0.3 * x * x - 0.3 * x * x * x);
+    int range;
+    std::cout << "Enter a Monte Carlo iteration range: ";
+    std::cin >> range;
+    std::cout << "Monte Carlo Estimation: " << simpleMonteCarlo(range);
+    return 0;
 }
